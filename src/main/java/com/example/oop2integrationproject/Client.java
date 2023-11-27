debc2112988631cc90912daa1a39632411d29993
Hamza Mahi <le2029384@crc-lennox.qc.ca>
2023-11-26 22:02:57 -0500

    Changed classes to suite next coding

--- src/main/java/com/example/oop2integrationproject/Client.java
+++ src/main/java/com/example/oop2integrationproject/Client.java
@@ -1,4 +1,35 @@
 package com.example.oop2integrationproject;
 
+import java.util.Objects;
+
 public class Client {
+    private final String userID;
+    private final String name;
+    private final String email;
+    private final String password;
+
+    public Client(String userID, String name, String email, String password) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getClientID() {
+        return userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
 }
--- src/main/java/com/example/oop2integrationproject/LoginController.java
+++ src/main/java/com/example/oop2integrationproject/LoginController.java
@@ -1,14 +1,29 @@
 package com.example.oop2integrationproject;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 
 public class LoginController {
 
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
 
     public void SignUpButtonClicked(ActionEvent actionEvent) {
 
     }
 
     public void loginButtonClicked(ActionEvent actionEvent) {
+
     }
 }
+
+
+
+
+
